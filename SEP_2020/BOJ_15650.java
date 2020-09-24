da3134527fa9ac03e0f7fd7eb49c8a40c17759f7
kong0527 <o____ri@naver.com>
2020-09-24 11:42:32 +0900

    solve 15650

---
+++ SEP_2020/BOJ_15650.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_15650 {
+	public static int N, M;
+	public static int[] arr;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String st[] = br.readLine().split(" ");
+		N = Integer.parseInt(st[0]);
+		M = Integer.parseInt(st[1]);
+		arr = new int[M];
+		
+		solve(1, 0);
+	}
+	
+	public static void solve(int start, int depth) {
+		if (depth == M) {
+			for (int val : arr) {
+				System.out.print(val + " ");
+			}
+			System.out.println();
+			return ;
+		}
+		
+		for (int i = start; i <= N; i++) {
+			arr[depth] = i;
+			solve(i + 1, depth + 1);
+		}
+	}
+}
