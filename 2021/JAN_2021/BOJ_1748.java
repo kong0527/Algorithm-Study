3574a5fbd8e75151c03f7a51e80ca517bb82eb7b
kong0527 <o____ri@naver.com>
2021-01-07 13:45:55 +0900

    solve 1748

---
+++ 2021/JAN_2021/BOJ_1748.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ_1748 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(solve(Integer.parseInt(s), s.length()));
+
+    }
+    public static int solve(int num, int length) {
+        if (length == 1) {
+            return num;
+        }
+
+        int ans = 0;
+        String flag = "";
+        int prev = 0;
+
+        for (int i = 1; i < length; i++) {
+            flag += "9";
+            int tmp = Integer.parseInt(flag);
+            ans += (tmp - prev) * i;
+            prev = tmp;
+        }
+
+        int tmp = Integer.parseInt(flag) + 1;
+        ans += (num - tmp + 1) * length;
+
+        return ans;
+    }
+}
