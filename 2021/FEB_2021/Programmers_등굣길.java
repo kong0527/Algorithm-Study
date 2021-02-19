45bd77b05deda54b8b678ab459b4deeb27af2748
kong0527 <o____ri@naver.com>
2021-02-19 23:03:10 +0900

    solve Programmers_등굣길

---
+++ 2021/FEB_2021/Programmers_등굣길.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int dp[][] = new int[n][m];
+        dp[0][0] = 1;
+
+
+        for (int i = 0; i < puddles.length; i++) {
+            int x = puddles[i][0];
+            int y = puddles[i][1];
+            dp[y-1][x-1] = -1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if ((i == 0 && j == 0) || dp[i][j] == -1) {
+                    continue;
+                }
+
+                if (i == 0) {
+                    dp[i][j] = dp[i][j-1];
+                }
+
+                else if (j == 0) {
+                    dp[i][j] = dp[i-1][j];
+                }
+
+                else {
+                    if (dp[i-1][j] != -1) {
+                        dp[i][j] += dp[i-1][j] % 1000000007;
+                    }
+                    if (dp[i][j-1] != -1) {
+                        dp[i][j] += dp[i][j-1] % 1000000007;
+                    }
+                }
+            }
+        }
+
+        return dp[n-1][m-1] % 1000000007;
+    }
+}
