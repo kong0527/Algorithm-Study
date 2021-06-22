d10a9ddcc5cf05939da7904c6d6efbf0798b7198
kong0527 <o____ri@naver.com>
2021-06-22 20:43:11 +0900

    solve Programmers_가운데 글자 가져오기

---
+++ 2021/JUNE_2021/Programmers_가운데 글자 가져오기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+
+        if (s.length() % 2 == 1) {
+            answer = Character.toString(s.charAt(s.length()/2));
+        }
+
+        else {
+            answer = Character.toString(s.charAt(s.length()/2-1)) +
+                    Character.toString(s.charAt(s.length()/2));
+        }
+
+        return answer;
+    }
+}
